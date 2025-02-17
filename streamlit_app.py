3a1f6014fce2bdf3de16450fca3f5d583ad83a2a
Saiidazizkhon Abdullaev <abdullaev.aziz1999@gmail.com>
2025-02-17 19:37:46 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,3 +40,24 @@ fig2 = px.histogram(
     title='Distribution of Body Mass'
 )
 st.plotly_chart(fig2)
+
+with st.expander('input features'):
+  st.write('**Input penguin**')
+  st.dataframe(input_df)
+  st.write('**Combined penguins data** (input row + original data)')
+  st.dataframe(input_penguins)
+encode = ['island', 'sex']
+df_penguins = pd.get_dummies(input_penguins, prefix = encode)
+
+X = df_penguins[:1]
+
+target_mapper = {'Adelie': 0, 'Chinstrap' : 1, 'Gentoo': 2}
+def target_encode(val):
+  return target_mapper[val]
+y = y_raw.apply(target_encode)
+
+with st.expander('Data preparation'):
+  st.write('**Encoded X (Input penguins)**')
+  st.dataframe(input_row)
+  st.write('**Encoded y**')
+  st.write(y)
