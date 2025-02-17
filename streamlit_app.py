f7e81907806bf16528e6a1514521575ffcd16665
Saiidazizkhon Abdullaev <abdullaev.aziz1999@gmail.com>
2025-02-17 18:52:45 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,14 @@ import streamlit as st
 st.title('🎈 Dash')
 
 st.write('Новый Дэш')
+
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+
+with st.expamder('Data'):
+  st.write()
+  X_raw = df.drop('Specoes', axis = 1)
+  st.dataFrame(X_raw)
+
+  st.write('y')
+  y_raw = df.species
+  st.dataframe(y_raw)
